eb20739794e7226ba91fd501c0a0ad1af1d5de75
Andy Whelan <andrew.whelan@disney.com>
2024-12-17 14:09:43 -0800

    fs for telescope + install ruff

--- init.lua
+++ init.lua
@@ -275,6 +275,7 @@ require('lazy').setup({
         --   },
         -- },
         -- pickers = {}
+        vim.keymap.set('n', '<leader>fs', ':Telescope find_files<cr>'),
         extensions = {
           ['ui-select'] = {
             require('telescope.themes').get_dropdown(),
@@ -537,6 +538,9 @@ require('lazy').setup({
       require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 
       require('mason-lspconfig').setup {
+        ensure_installed = {
+          'ruff',
+        },
         handlers = {
           function(server_name)
             local server = servers[server_name] or {}
