4039630468360490176965207308984c3bf4c3c5
Andy Whelan <Andrew.Whelan@disney.com>
2026-03-08 12:32:04 -0700

    diffview

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,14 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+return {
+  {
+    'sindrets/diffview.nvim',
+    cmd = { 'DiffviewOpen', 'DiffviewFileHistory' },
+    keys = {
+      { '<leader>gd', '<cmd>DiffviewOpen<cr>', desc = 'Diffview Open' },
+      { '<leader>gh', '<cmd>DiffviewFileHistory %<cr>', desc = 'File History' },
+      { '<leader>gH', '<cmd>DiffviewFileHistory<cr>', desc = 'Repo History' },
+    },
+  },
+}
